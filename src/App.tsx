7f4322681c4a0d5fa4ee390d6c1c965f3fc11381
helman101 <helmanandresortegon@gmail.com>
2024-03-02 20:13:07 +0100

    FEAT: create pages & layouts and organize them with routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { Route, Routes } from 'react-router-dom'
+
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
+import SignupForm from './_auth/forms/SignupForm'
+import SigninForm from './_auth/forms/SigninForm'
+import { Home } from './_root/pages'
 import './globals.scss'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <main className='d-flex vh-100'>
+      <Routes>
+        {/* Public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SigninForm />}/>
+          <Route path='/sign-up' element={<SignupForm />}/>
+        </Route>
+
+        {/* Private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />}/>
+        </Route>
+      </Routes>
+    </main>
   )
 }
