cd77e4ccd5e86f23836ee60bcaed47150ec13d66
helman101 <helmanandresortegon@gmail.com>
2024-03-06 17:20:32 +0100

    FEAT: condition AuthLayout by authentication and styled children wrapper

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,6 +1,18 @@
+import { Outlet, Navigate } from "react-router-dom"
+
 const AuthLayout = () => {
+  const isAuthenticated = false
+
   return (
-    <div>AuthLayout</div>
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <section className="container d-flex align-items-center justify-content-center">
+          <Outlet />
+        </section>
+      )}
+    </>
   )
 }
 
