1f359ac6840f12935e3a23c61ba8b75b06ad7e94
helman101 <helmanandresortegon@gmail.com>
2024-03-07 18:02:00 +0100

    FEAT: make username optional

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 export const SignupValidation = yup.object({
   name: yup.string().min(3, 'Name must be at least 3 characters long').required(),
-  username: yup.string().min(3, 'Username must be at least 3 characters long').required(),
+  username: yup.string().min(3, 'Username must be at least 3 characters long'),
   email: yup.string().email().required('Email is required'),
   password: yup.string().min(8, 'Password must be at least 8 characters long').required()
 }).required();
