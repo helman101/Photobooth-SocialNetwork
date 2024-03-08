6b5095aad7d5f0303d9d88d88d040935839380dc
helman101 <helmanandresortegon@gmail.com>
2024-03-08 14:39:05 +0100

    FEAT: create saveUserToDB function & complete createUserAccount function with it

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,9 +1,11 @@
 import { ID } from "appwrite"
 import { INewUser } from "../../types"
-import { account } from "./config"
+import { account, appwriteConfig, avatars, databases } from "./config"
 
+// Create user account
 export async function createUserAccount(user: INewUser) {
   try {
+    // Authenticate user information
     const newAccount = await account.create(
       ID.unique(),
       user.email,
@@ -11,9 +13,44 @@ export async function createUserAccount(user: INewUser) {
       user.name
     )
 
-    return newAccount
+    if (!newAccount) throw Error
+
+    // Create an avatar (profile img) for users with appwrite "avatar" tool
+    const avatarUrl = avatars.getInitials(user.name)
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl
+    })
+
+    return newUser
   } catch (error) {
     console.log(error)
     return error
+  }
+}
+
+// Add user to the Users database
+export async function saveUserToDB(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    )
+
+    return newUser
+  } catch (error) {
+    console.log(error)
   }
 }
