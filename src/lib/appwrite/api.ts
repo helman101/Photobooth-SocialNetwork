4c6e01a15b4e1663f92f96ed52544ae363a13034
helman101 <helmanandresortegon@gmail.com>
2024-03-21 10:52:05 +0100

    FEAT: add signInAccount & getCurrentUser API calls

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,5 +1,5 @@
-import { ID } from "appwrite"
-import { INewUser, IUserFromAccount } from "../../types"
+import { ID, Query } from "appwrite"
+import { INewUser, ISessionUser, IUserFromAccount } from "../../types"
 import { account, appwriteConfig, avatars, databases } from "./config"
 
 // Create user account
@@ -44,6 +44,41 @@ export async function saveUserToDB(user: IUserFromAccount) {
     )
 
     return newUser
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Create a new session
+export async function signInAccount(user: ISessionUser) {
+  try {
+    const session = await account.createEmailSession(user.email, user.password)
+
+    return session
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Get the current user
+export async function getCurrentUser() {
+  try {
+    // Get account
+    const currentAccount = await account.get()
+
+    // Check if current account exists in database
+    if(!currentAccount) throw Error;
+
+    // If exists try to retrieve account from database
+    const currentUser = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal('accountId', currentAccount.$id)]
+    )
+
+    if (!currentUser) throw Error;
+
+    return currentUser.documents[0]
   } catch (error) {
     console.log(error)
   }
