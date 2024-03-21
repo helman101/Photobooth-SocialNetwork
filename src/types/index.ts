b583ff792013be19c83ff508a0e40c7e124b38e8
helman101 <helmanandresortegon@gmail.com>
2024-03-21 10:53:17 +0100

    FEAT: add IUser, ISessionUser & IContextType new interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import React from "react";
+
 export interface INewUser {
   name: string,
   email: string,
@@ -10,4 +12,26 @@ export interface IUserFromAccount {
   name: string;
   imageUrl: URL;
   username?: string;
+}
+
+export interface IUser {
+  id: string;
+  email: string;
+  name: string;
+  bio: string,
+  imageUrl: string;
+  username?: string;
+}
+export interface ISessionUser {
+  email: string
+  password: string
+}
+
+export interface IContextType {
+  user: IUser
+  isLoading: boolean
+  setUser: React.Dispatch<React.SetStateAction<IUser>>
+  isAuthenticated: boolean
+  setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>
+  checkAuthUser: () => Promise<boolean>
 }
